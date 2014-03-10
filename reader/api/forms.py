d15e792924d46df8922f06a8895c27b4b96c443c
Martin Skow Røed <msroed@gmail.com>
2014-03-10 22:49:19 +0100

    login/logout

---
+++ reader/api/forms.py
@@ -0,0 +1,15 @@
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, UserChangeForm
+from .models import User
+from django import forms
+
+
+class AuthenticateForm(AuthenticationForm):
+    username = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={'placeholder': 'Username'}))
+    password = forms.CharField(required=True, widget=forms.widgets.PasswordInput(attrs={'placeholder': 'Password'}))
+ 
+    def is_valid(self):
+        form = super(AuthenticateForm, self).is_valid()
+        for f, error in self.errors.iteritems():
+            if f != '__all__':
+                self.fields[f].widget.attrs.update({'class': 'error', 'value': strip_tags(error)})
+        return form
--- reader/api/views.py
+++ reader/api/views.py
@@ -1,4 +1,21 @@
 from django.shortcuts import render
+from .models import User
+from django.contrib.auth import authenticate, login, logout
+from .forms import AuthenticateForm
+import json
 
 def index(request):
 	return render(request, 'index.html')
+
+def login(request):
+	user = None
+	if request.POST:
+		form = AuthenticateForm(request.POST)
+		if form.is_valid():
+			login(request, form.get_user())
+			user = form.get_user()
+	return json.dumps({'user': user})
+
+def logout_view(request):
+	logout(request)
+	return json.dumps({'status': 200})
